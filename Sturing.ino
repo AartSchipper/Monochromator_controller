e1043820615f9bd39f322fbacbcd8193083b0fbb
Aart Schipper <aart.schipper@gmail.com>
2019-02-07 12:45:16 +0100

    Overspeed protection

--- Sturing.ino
+++ Sturing.ino
@@ -4,12 +4,13 @@
  * 
  */
 
-const int STEP = 3;
-const int DIR = 2; 
-const float STEPS_NM = 96.2463;
+const int STEP = 3; // pin 
+const int DIR = 2;  // pin
+const float STEPS_NM = 96.2463; // steps per nm 
+const int MIN_stepTime = 400; // min. 400 us between steps
 
-const int UP = 1; // langere golflengte = DIR 
-#define SIGNAL A0
+const int UP = 1; // Direction
+#define SIGNAL A0 // pin
 
 void setup() { 
   // initialize digital pin LED_BUILTIN as an output.
@@ -26,25 +27,26 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
- int adc; 
  for (int i = 0; i < 100; i++) {
    Serial.print("Meting:"); Serial.println(i); 
    
-   int current_position = move_steps(-110);  
+   int current_position = move_steps(-110, 400);  
    Serial.print("Positie: "); Serial.println(current_position); 
    Serial.print("Golflengte: "); Serial.println(current_position / STEPS_NM); 
    
    delay(10); 
-   
+
+   int adc; 
    adc = analogRead(SIGNAL);
    Serial.print(adc); Serial.println(","); 
  }
   while(1); 
 }
 
-
-int move_steps (int amount) {
+int move_steps (int amount, int stepTime) { 
    static int current_position = 200 * STEPS_NM; 
+
+   if (stepTime < MIN_stepTime) return current_position; // do not overspeed
    
    current_position += amount;
    
@@ -52,18 +54,18 @@ int move_steps (int amount) {
     if (amount > 0) {
       digitalWrite (DIR, UP);
       digitalWrite (STEP, HIGH);
-      delay(1);
+      delayMicroseconds(stepTime);
       digitalWrite (STEP, LOW);
-      delay(1); 
+      delayMicroseconds(stepTime); 
       amount--; 
     }
 
     if (amount < 0) {
       digitalWrite (DIR, !UP);
       digitalWrite (STEP, HIGH);
-      delay(1);
+      delayMicroseconds(stepTime);
       digitalWrite (STEP, LOW);
-      delay(1); 
+      delayMicroseconds(stepTime); 
       amount++; 
     }
    }
