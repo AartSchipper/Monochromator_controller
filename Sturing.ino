7f8dcb779e1fe640f7d54dba659f38ce860ddd0d
Aart Schipper <aart.schipper@gmail.com>
2019-02-07 12:09:05 +0100

    Correct steps to nm conversion

--- Sturing.ino
+++ Sturing.ino
@@ -5,37 +5,70 @@
  */
 
 const int STEP = 3;
-const int DIR = 2; // in Fullstep: 208 nm / 10000 stappen = 0.0208 nm per stap
+const int DIR = 2; 
+const float STEPS_NM = 96.2463;
+
 const int UP = 1; // langere golflengte = DIR 
 #define SIGNAL A0
 
-void setup() {
+void setup() { 
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(STEP, OUTPUT);
   pinMode(DIR, OUTPUT);
-  digitalWrite (DIR, UP);
+  digitalWrite (DIR, !UP);
   
   pinMode(SIGNAL, INPUT);
   analogReference(DEFAULT); 
   Serial.begin(9600); 
+  while (!Serial) { // wait
+    } 
 }
 
 // the loop function runs over and over again forever
 void loop() {
  int adc; 
- for (int i = 0; i < 2000; i++) {
-   move_10(); 
+ for (int i = 0; i < 100; i++) {
+   Serial.print("Meting:"); Serial.println(i); 
+   
+   int current_position = move_steps(-110);  
+   Serial.print("Positie: "); Serial.println(current_position); 
+   Serial.print("Golflengte: "); Serial.println(current_position / STEPS_NM); 
+   
+   delay(10); 
+   
    adc = analogRead(SIGNAL);
    Serial.print(adc); Serial.println(","); 
  }
   while(1); 
 }
 
-void move_10() { 
-  for (int i = 0; i < 10; i++) {
-    digitalWrite (STEP, HIGH);
-    delay(10);
-    digitalWrite (STEP, LOW);
-    delay(10); 
-  }
+
+int move_steps (int amount) {
+   static int current_position = 200 * STEPS_NM; 
+   
+   current_position += amount;
+   
+   while (amount) {
+    if (amount > 0) {
+      digitalWrite (DIR, UP);
+      digitalWrite (STEP, HIGH);
+      delay(1);
+      digitalWrite (STEP, LOW);
+      delay(1); 
+      amount--; 
+    }
+
+    if (amount < 0) {
+      digitalWrite (DIR, !UP);
+      digitalWrite (STEP, HIGH);
+      delay(1);
+      digitalWrite (STEP, LOW);
+      delay(1); 
+      amount++; 
+    }
+   }
+    
+  return current_position; 
 }
+
+
